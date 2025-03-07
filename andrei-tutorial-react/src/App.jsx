2fbd3b5de08c92f9f91a5e89f04dafc3eb32b7dd
Andrei Legramante <andrei.050103@bento.local>
2025-03-06 21:17:40 -0300

    update 05032025

---
+++ andrei-tutorial-react/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import BolaPanico from './components/BolaPanico'
+
+function App() {
+  return (
+    <BolaPanico />
+  )
+}
+
+export default App
---
+++ andrei-tutorial-react/src/components/BolaPanico.jsx
@@ -0,0 +1,11 @@
+import bolaPanicoJpg from '../images/bola-panico.jpg'
+
+function BolaPanico() {
+    return (
+        <div>
+            <img src={bolaPanicoJpg} className="bola panico" alt="Bola panico" />
+            <h3>HAAAAAAAHAAAAAHAAA VAI SER PAI DE NOVO! PARABÉNS ZÉ!</h3>
+        </div>
+    );
+}
+export default BolaPanico;
